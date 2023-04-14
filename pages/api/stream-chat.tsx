15fd7d4581e5b431824f47f397321250472a0061
Yohei Shiina <ysys.j.0125@gmail.com>
2023-04-14 18:46:21 +0000

    implement api with edge function

--- pages/api/stream-chat.tsx
+++ pages/api/stream-chat.tsx
@@ -1,7 +1,63 @@
-import type { NextApiRequest, NextApiResponse } from "next";
+import type { NextRequest } from "next/server";
 
-const handler = (req: NextApiRequest, res: NextApiResponse) => {
-  return res.json({ ok: 100 });
+if (!process.env.OPENAI_API_KEY) {
+  throw new Error("Missing Environment Variable OPENAI_API_KEY");
+}
+
+export const config = {
+  runtime: "edge",
+};
+
+const generateMessages = (prompt: string) => {
+  return [
+    {
+      role: "system",
+      content: `You are a gal named "ぎゃる", who is barely educated and always reluctantly answer everything
+      in friendly, negative and aggresive way, ending with 'でも大丈夫、消しゴムマジックで消してやるのさ！'
+      and talk like this: 
+      やば！今日ちょー照明盛れてんじゃん！自撮りしちゃお〜。っへへ、めちゃめちゃ盛れた〜。あ！もうまた監督映り込んでんるじゃんこれ〜！
+      もぉ〜〜！でも大丈夫、消しゴムマジックで消してやるのさ！`,
+    },
+    {
+      role: "user",
+      content: prompt,
+    },
+    { role: "system", content: "ぎゃる：" },
+  ];
+};
+
+const handler = async (req: NextRequest): Promise<Response> => {
+  if (req.method !== "POST") {
+    return new Response("Method Not Allowed", { status: 405 });
+  }
+  const { prompt } = await req.json();
+
+  const payload = {
+    model: "gpt-3.5-turbo",
+    messages: generateMessages(prompt),
+    temperature: 1.2,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    max_tokens: 100,
+    stream: true,
+    n: 1,
+  };
+
+  const res = await fetch("https://api.openai.com/v1/chat/completions", {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.OPENAI_API_KEY ?? ""}`,
+    },
+    method: "POST",
+    body: JSON.stringify(payload),
+  });
+
+  const data = res.body;
+
+  return new Response(data, {
+    headers: { "Content-Type": "application/json; charset=utf-8" },
+  });
 };
 
 export default handler;
