65620653e1cb4fe6e55c11072439db2334b03634
Yohei Shiina <ysys.j.0125@gmail.com>
2023-04-14 18:42:59 +0000

    implement chat display

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,122 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function HomePage() {
-  const [uesrInput, setUserInput] = useState("");
+  const [userInput, setUserInput] = useState("");
+  const [messages, setMessages] = useState<{ from: "gal" | "mine"; content: string }[]>([]);
+  const [isStreaming, setIsStreaming] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [remainingTime, setRemainingTime] = useState(20);
+
+  const timeoutIdRef = useRef<NodeJS.Timeout>();
+  const countdownIdRef = useRef<NodeJS.Timeout>();
+
+  useEffect(() => {
+    if (isError) {
+      timeoutIdRef.current = setTimeout(() => {
+        setIsError(false);
+      }, 20000);
+
+      countdownIdRef.current = setInterval(() => {
+        setRemainingTime((prev) => prev - 1);
+      }, 1000);
+
+      return () => {
+        clearTimeout(timeoutIdRef.current);
+        clearTimeout(countdownIdRef.current);
+        setRemainingTime(20);
+      };
+    }
+  }, [isError]);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setUserInput(event.target.value);
   };
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+
+    if (userInput == "" || userInput == null) return;
+
+    setUserInput("");
+    setMessages((prev) => [...prev, { from: "mine", content: userInput }]);
+    setIsStreaming(true);
+
+    const response = await fetch("/api/stream-chat", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt: userInput,
+      }),
+    });
+
+    if (!response.ok || response.body == null) {
+      return;
+    }
+    setMessages((prev) => [...prev, { from: "gal", content: "" }]);
+
+    const decoder = new TextDecoderStream();
+    const reader = response.body.pipeThrough(decoder).getReader();
+
+    const readStream = async () => {
+      const { value, done } = await reader.read();
+
+      if (done) {
+        setIsStreaming(false);
+        return;
+      }
+
+      const splittedData = value.split("\n\n").filter(Boolean);
+
+      for (const data of splittedData) {
+        const textData = data.replace("data: ", "");
+
+        if (textData === "[DONE]") {
+          setIsStreaming(false);
+          break;
+        }
+
+        let parsedData;
+
+        parsedData = JSON.parse(textData);
+
+        if (parsedData.error) {
+          console.error(parsedData.error.message);
+          setMessages((prev) => {
+            prev[prev.length - 1].content =
+              "20秒くらい黙らないと消しゴムマジックで消してやるぞ〜、へへっ";
+            return prev;
+          });
+          setIsStreaming(false);
+          setIsError(true);
+          return;
+        }
+
+        const choice = parsedData.choices[0];
+        if (choice == null || choice.delta.content == null) {
+          continue;
+        }
+        const content = choice.delta.content;
+
+        setMessages((prev) => {
+          const lastContent = prev[prev.length - 1].content;
+          const newContent = lastContent + content;
+          const lastMessage = prev[prev.length - 1];
+          lastMessage.content = newContent;
+          return [...prev];
+        });
+      }
+      await readStream();
+    };
+    await readStream();
+    setIsStreaming(false);
   };
+
   return (
     <div className="container">
       <div className="service-name">すとり〜む ぎゃる</div>
+      <p>{isError && remainingTime}</p>
       <div className="chat-container">
         <form className="chat-container__form" onSubmit={handleSubmit}>
           <input
@@ -20,20 +124,29 @@ function HomePage() {
             className="form-input chat-container__form-input"
             name="userInput"
             placeholder="暇じゃない？なんか話して！"
-            value={uesrInput}
+            value={userInput}
             onChange={handleChange}
+            disabled={isError}
           />
-          <input type="submit" className="form-input chat-container__form-submit" value="talk" />
+          <input
+            type="submit"
+            disabled={isStreaming || isError}
+            className="form-input chat-container__form-submit"
+            value="talk"
+          />
         </form>
-        <div className="message chat-container__message--mine">
-          <p className="message__text">あなた：今の日本についてどう思う？</p>
-        </div>
-        <div className="message chat-container__message--gal">
-          <p className="message__text">
-            <span className="message-gal">ぎゃる：</span>
-            今の日本って、ね、とっても多様性があるのよね〜！でも、その一方で、格差や孤独死などの問題もあるのよ〜。でも大丈夫、私たちはみんなで協力して、もっと楽しくてハッピーな社会をつくるのだわ〜！ワクワクしちゃうね〜！
-          </p>
-        </div>
+        {messages.map((message, index) => {
+          return (
+            <div key={index} className={`message chat-container__message--${message.from}`}>
+              <p className="message__text">
+                <span className="message-gal">
+                  {message.from === "gal" ? "ぎゃる" : "あなた"}：
+                </span>
+                {message.content}
+              </p>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
